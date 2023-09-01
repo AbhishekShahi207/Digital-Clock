6e4c1416f1fd29e0dcc6d786cdf415a4faf9eb4c
AbhishekShahi207 <shahiabhi1005@gmail.com>
2023-09-01 14:07:53 +0530

    initial commit

---
+++ date.js
@@ -0,0 +1,17 @@
+
+const time=document.getElementById("time");
+const timeformat=document.getElementById("timeformat");
+
+
+document.addEventListener("DOMContentLoaded",()=>{
+    setInterval(showTime, 1000);
+})
+const showTime=()=>{
+    let date= new Date();
+ let hr=date.getHours();
+ let min=date.getMinutes();
+ let sec=date.getSeconds();
+ time.innerHTML=`${hr}:${min}:${sec}`
+
+
+}
